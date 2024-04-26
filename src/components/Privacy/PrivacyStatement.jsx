6beb589bb99d002b3e73b5aac53e548f75e684db
jordan-watson <jordanwatson000@gmail.com>
2024-04-26 17:26:20 +0100

    complete privacy statement

--- src/components/Privacy/PrivacyStatement.jsx
+++ src/components/Privacy/PrivacyStatement.jsx
@@ -1,274 +1,274 @@
-import "./PrivacyStatement"
+import "./PrivacyStatement.css"
 
 const PrivacyStatement = () => {
   return (
-    <>
-      <body class="c17 doc-content">
-        <p class="c7">
-          <span class="c14">&#65279;</span>
-          <span class="c2">&nbsp;</span>
+    <main className="master">
+      <body className="c17 doc-content">
+        <p className="c7">
+          <span className="c14">&#65279;</span>
+          <span className="c2">&nbsp;</span>
         </p>
-        <ol class="c5 lst-kix_list_1-1 start" start="1">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">GatefoldWax customer privacy notice</span>
+        <ol className="c5 lst-kix_list_1-1 start" start="1">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">GatefoldWax customer privacy notice</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             This privacy notice tells you what to expect us to do with your
             personal information.
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="2">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Our contact details</span>
+        <ol className="c5 lst-kix_list_1-1" start="2">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Our contact details</span>
             </h2>
           </li>
         </ol>
-        <ol class="c5 lst-kix_list_1-2 start" start="1">
-          <li class="c3">
-            <h3 style="display: inline">
-              <span class="c16">Email</span>
+        <ol className="c5 lst-kix_list_1-2 start" start="1">
+          <li className="c3">
+            <h3 style={{display:"inline"}}>
+              <span className="c16">Email</span>
             </h3>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">gatefoldwax@gmail.com</span>
+        <p className="c0">
+          <span className="c2">gatefoldwax@gmail.com</span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="3">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">What information we collect, use, and why</span>
+        <ol className="c5 lst-kix_list_1-1" start="3">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">What information we collect, use, and why</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             We collect or use the following information for{" "}
           </span>
-          <span class="c4">
+          <span className="c4">
             the operation of customer accounts and guarantees
           </span>
-          <span class="c2">:</span>
+          <span className="c2">:</span>
         </p>
-        <ul class="c5 lst-kix_list_2-0 start">
-          <li class="c0 c11 li-bullet-0">
-            <span class="c2">Names and contact details</span>
+        <ul className="c5 lst-kix_list_2-0 start">
+          <li className="c0 c11 li-bullet-0">
+            <span className="c2">Names and contact details</span>
           </li>
         </ul>
-        <ul class="c5 lst-kix_list_3-0 start">
-          <li class="c0 c11 li-bullet-0">
-            <span class="c2">
+        <ul className="c5 lst-kix_list_3-0 start">
+          <li className="c0 c11 li-bullet-0">
+            <span className="c2">
               Account information, including registration details
             </span>
           </li>
         </ul>
-        <ul class="c5 lst-kix_list_4-0 start">
-          <li class="c0 c11 li-bullet-0">
-            <span class="c2">Information used for security purposes</span>
+        <ul className="c5 lst-kix_list_4-0 start">
+          <li className="c0 c11 li-bullet-0">
+            <span className="c2">Information used for security purposes</span>
           </li>
         </ul>
-        <ol class="c5 lst-kix_list_1-1 start" start="1">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Lawful bases</span>
+        <ol className="c5 lst-kix_list_1-1 start" start="1">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Lawful bases</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             Our lawful bases for collecting or using personal information for{" "}
           </span>
-          <span class="c4">
+          <span className="c4">
             the operation of customer accounts and guarantees{" "}
           </span>
-          <span class="c2">are:</span>
+          <span className="c2">are:</span>
         </p>
-        <ul class="c5 lst-kix_list_5-0 start">
-          <li class="c0 c11 li-bullet-0">
-            <span class="c2">Consent</span>
+        <ul className="c5 lst-kix_list_5-0 start">
+          <li className="c0 c11 li-bullet-0">
+            <span className="c2">Consent</span>
           </li>
         </ul>
-        <ol class="c5 lst-kix_list_1-1 start" start="1">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Where we get personal information from</span>
+        <ol className="c5 lst-kix_list_1-1 start" start="1">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Where we get personal information from</span>
             </h2>
           </li>
         </ol>
-        <ul class="c5 lst-kix_list_6-0 start">
-          <li class="c0 c11 li-bullet-0">
-            <span class="c2">People directly</span>
+        <ul className="c5 lst-kix_list_6-0 start">
+          <li className="c0 c11 li-bullet-0">
+            <span className="c2">People directly</span>
           </li>
         </ul>
-        <ol class="c5 lst-kix_list_1-1 start" start="1">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">How long we keep information</span>
+        <ol className="c5 lst-kix_list_1-1 start" start="1">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">How long we keep information</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c18">
+        <p className="c0">
+          <span className="c18">
             GatefoldWax will keep user information for the duration that a user
             is deemed actively engaging with our services. A user is considered
             to be actively engaged with the service if they have logged in
             within a 12 month period.
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="2">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Who we share information with</span>
+        <ol className="c5 lst-kix_list_1-1" start="2">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Who we share information with</span>
             </h2>
           </li>
         </ol>
-        <ol class="c5 lst-kix_list_1-2 start" start="1">
-          <li class="c3">
-            <h3 style="display: inline">
-              <span class="c16">Data processors</span>
+        <ol className="c5 lst-kix_list_1-2 start" start="1">
+          <li className="c3">
+            <h3 style={{display:"inline"}}>
+              <span className="c16">Data processors</span>
             </h3>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c4">Supabase</span>
+        <p className="c0">
+          <span className="c4">Supabase</span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             This data processor does the following activities for us: Process
             user information for account verification and the provision of data
             handling services.
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="3">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Sharing information outside the UK</span>
+        <ol className="c5 lst-kix_list_1-1" start="3">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Sharing information outside the UK</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             Where necessary, we may transfer personal information outside of the
             UK. When doing so, we comply with the UK GDPR, making sure
             appropriate safeguards are in place. Please contact us for more
             information.
           </span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             Where necessary, our data processors may share personal information
             outside of the UK. When doing so, they comply with the UK GDPR,
             making sure appropriate safeguards are in place. Please contact us
             for more information.
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="4">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Your data protection rights</span>
+        <ol className="c5 lst-kix_list_1-1" start="4">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Your data protection rights</span>
             </h2>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             Under data protection law, you have rights including:
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right of access</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right of access</span>
+          <span className="c2">
             &nbsp;- You have the right to ask us for copies of your personal
             data.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to rectification</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to rectification</span>
+          <span className="c2">
             &nbsp;- You have the right to ask us to rectify personal data you
             think is inaccurate. You also have the right to ask us to complete
             information you think is incomplete.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to erasure</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to erasure</span>
+          <span className="c2">
             &nbsp;- You have the right to ask us to erase your personal data in
             certain circumstances.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to restriction of processing</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to restriction of processing</span>
+          <span className="c2">
             &nbsp;- You have the right to ask us to restrict the processing of
             your personal data in certain circumstances.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to object to processing</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to object to processing</span>
+          <span className="c2">
             &nbsp;- You have the right to object to the processing of your
             personal data in certain circumstances.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to data portability</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to data portability</span>
+          <span className="c2">
             &nbsp;- You have the right to ask that we transfer the personal data
             you gave us to another organisation, or to you, in certain
             circumstances.
           </span>
         </p>
-        <p class="c0">
-          <span class="c4">Your right to withdraw consent</span>
-          <span class="c2">
+        <p className="c0">
+          <span className="c4">Your right to withdraw consent</span>
+          <span className="c2">
             &nbsp;&ndash; When we use consent as our lawful basis you have the
             right to withdraw your consent.
           </span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             You don&rsquo;t usually need to pay a fee to exercise your rights.
             If you make a request, we have one calendar month to respond to you.
           </span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             To make a data protection rights request, please contact us using
             the contact details at the top of this privacy notice.
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-2" start="2">
-          <li class="c3">
-            <h3 style="display: inline">
-              <span class="c16">How to complain</span>
+        <ol className="c5 lst-kix_list_1-2" start="2">
+          <li className="c3">
+            <h3 style={{display:"inline"}}>
+              <span className="c16">How to complain</span>
             </h3>
           </li>
         </ol>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             If you have any concerns about our use of your personal data, you
             can make a complaint to us using the contact details at the top of
             this privacy notice.
           </span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             If you remain unhappy with how we&rsquo;ve used your data after
             raising a complaint with us, you can also complain to the ICO.
           </span>
         </p>
-        <p class="c0">
-          <span class="c2">
+        <p className="c0">
+          <span className="c2">
             The ICO&rsquo;s
             address:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </span>
         </p>
-        <p class="c0 c10">
-          <span class="c2">
+        <p className="c0 c10">
+          <span className="c2">
             Information Commissioner&rsquo;s Office
             <br />
             Wycliffe House
@@ -282,35 +282,35 @@ const PrivacyStatement = () => {
             SK9 5AF
           </span>
         </p>
-        <p class="c0 c10">
-          <span class="c2">Helpline number: 0303 123 1113</span>
+        <p className="c0 c10">
+          <span className="c2">Helpline number: 0303 123 1113</span>
         </p>
-        <p class="c0 c10">
-          <span class="c2">Website: </span>
-          <span class="c8">
+        <p className="c0 c10">
+          <span className="c2">Website: </span>
+          <span className="c8">
             <a
-              class="c15"
+              className="c15"
               href="https://www.google.com/url?q=https://ico.org.uk/make-a-complaint/&amp;sa=D&amp;source=editors&amp;ust=1714150738865064&amp;usg=AOvVaw35dTPmHyEuWYd_xjd3nYhT"
             >
               https://www.ico.org.uk/make-a-complaint
             </a>
           </span>
         </p>
-        <ol class="c5 lst-kix_list_1-1" start="5">
-          <li class="c6">
-            <h2 style="display: inline">
-              <span class="c12">Last updated</span>
+        <ol className="c5 lst-kix_list_1-1" start="5">
+          <li className="c6">
+            <h2 style={{display:"inline"}}>
+              <span className="c12">Last updated</span>
             </h2>
           </li>
         </ol>
-        <p class="c9">
-          <span class="c2">26 April 2024</span>
+        <p className="c9">
+          <span className="c2">26 April 2024</span>
         </p>
-        <p class="c7 c13">
-          <span class="c1"></span>
+        <p className="c7 c13">
+          <span className="c1"></span>
         </p>
       </body>
-    </>
+    </main>
   );
 };
 
--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,10 +1,9 @@
-
+import PrivacyStatement from "../components/Privacy/PrivacyStatement";
 const Privacy = () => {
   return (
     <>
       <h1 className="heading">Privacy Statement</h1>
-
-  
+      <PrivacyStatement />
     </>
   );
 };
