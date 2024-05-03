dd721ae9336840b9224e1e86bc9f3df58204ee05
Jordan <jordanwatson000@gmail.com>
2024-05-03 11:45:52 +0100

    add logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 const Home = () => {
-
-const homeDescription = "Add reviews and give us your hot take on the latest releases or on an old classic. \n \nFollow your friends and see what they think. \n \nCheck the latest feed for the newest reviews and see what's being said! \n \nWax builds the conversation around music, letting the listener have their say on what's worth spinning and what's worth binning."
+  const homeDescription =
+    "Add reviews and give us your hot take on the latest releases or on an old classic. \n \nFollow your friends and see what they think. \n \nCheck the latest feed for the newest reviews and see what's being said! \n \nWax builds the conversation around music, letting the listener have their say on what's worth spinning and what's worth binning."
 
   return (
     <>
-      <h1 className="heading">WAX</h1>
-      <p className="homeDescription">
-       {homeDescription}
-      </p>
+      <div className="homeLogoDIV">
+        <img src="src/assets/Wax-logo-transparent.png" className="homeLogo"/>
+      </div>
+      <h1 className="heading">Welcome to WAX</h1>
+      <p className="homeDescription">{homeDescription}</p>
     </>
   )
 }
